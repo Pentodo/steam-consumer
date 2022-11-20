eebc2954579a71ad7d4d61dcc8c1b2d58a89c308
Pentodo <matheus.a.v.b@gmail.com>
2022-11-20 06:25:13 -0300

    implemented addAppDetails service

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, app_details } from '@prisma/client';
+import axios from 'axios';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
@@ -11,5 +12,24 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 		this.$on('beforeExit', async () => {
 			await app.close();
 		});
+	}
+
+	async addAppDetails(appids: Array<number>) {
+		const url = 'http://store.steampowered.com/api/appdetails/?appids=';
+		const responses: Array<any> = (
+			await Promise.all(appids.map(async (appid) => (await axios.get(url + appid)).data[appid]?.data))
+		).filter((app) => app);
+
+		const apps_details: Array<app_details> = responses.map((app) => ({
+			appid: app.steam_appid,
+			type: app.type,
+			release_date: app.release_date.date,
+			price: app.is_free ? 0 : app.price_overview?.initial || -1,
+			description: app.short_description,
+			header: app.header_image,
+			background: app.background,
+		}));
+
+		return this.app_details.createMany({ data: apps_details, skipDuplicates: true });
 	}
 }

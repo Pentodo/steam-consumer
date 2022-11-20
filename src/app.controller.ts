b5a9a09a6018694ca7434a25c30e89f1988ef943
Pentodo <matheus.a.v.b@gmail.com>
2022-11-20 06:25:50 -0300

    added app details endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Body, Put, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { app, genre } from '@prisma/client';
 
@@ -6,9 +6,25 @@ import { app, genre } from '@prisma/client';
 export class AppController {
 	constructor(private readonly prismaService: PrismaService) {}
 
-	@Get('apps')
+	@Get(['', 'apps'])
 	async getAllApps(): Promise<app[]> {
 		return this.prismaService.app.findMany();
+	}
+
+	@Get('apps/details')
+	async getAppsDetails(@Query('appids') appids: string): Promise<app[]> {
+		const ids: Array<number> = appids.split(',').map((num) => Number(num));
+		const query: any = {
+			where: { appid: { in: ids }, app_details: null },
+			include: { app_details: true },
+		};
+
+		await this.prismaService.addAppDetails(
+			(await this.prismaService.app.findMany(query)).map((app) => app.appid)
+		);
+
+		delete query.where.app_details;
+		return this.prismaService.app.findMany(query);
 	}
 
 	@Get('genres')
